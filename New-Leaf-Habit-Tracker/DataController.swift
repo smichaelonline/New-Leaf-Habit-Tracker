15f56e01794632eac6d931572c491fe0fb49b7c9
smichaelonline <stephaniemichael001@gmail.com>
2023-02-10 23:52:35 -0500

    Set up Habit model

---
+++ New-Leaf-Habit-Tracker/DataController.swift
@@ -0,0 +1,14 @@
+//
+//  DataController.swift
+//  New-Leaf-Habit-Tracker
+//
+//  Created by Stephanie Michael on 2/10/23.
+//
+
+import CoreData
+import SwiftUI
+
+class DataConroller: ObservedObject {
+  let container: NSPersistentCloudKitContainer
+  
+}
