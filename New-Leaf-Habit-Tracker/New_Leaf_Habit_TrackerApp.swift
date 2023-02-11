4b0887c5ac7a902cd25ee47f44f560c301315065
smichaelonline <stephaniemichael001@gmail.com>
2023-02-10 23:35:53 -0500

    Initial Commit

---
+++ New-Leaf-Habit-Tracker/New_Leaf_Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Leaf_Habit_TrackerApp.swift
+//  New-Leaf-Habit-Tracker
+//
+//  Created by Stephanie Michael on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct New_Leaf_Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
